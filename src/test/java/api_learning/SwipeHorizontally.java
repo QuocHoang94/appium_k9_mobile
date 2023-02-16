f10f18c66c71e15bd8a2333dd574b683206e7d6a
DESKTOP-BG9IMGR\Admin <tqhoangcnttb94@gmail.com>
2023-02-16 23:12:57 +0700

    Lesson_16

---
+++ src/test/java/api_learning/SwipeHorizontally.java
@@ -0,0 +1,81 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import platform.Platform;
+
+import java.time.Duration;
+
+public class SwipeHorizontally {
+
+    public static void main(String[] args) {
+
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+
+        try {
+            // Navigate to Forms screen
+            MobileElement navSwipeScreenBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Swipe"));
+            navSwipeScreenBtnElem.click();
+
+            // Wait until user is on Forms screen
+            WebDriverWait wait = new WebDriverWait(appiumDriver, 10L);
+            wait.until(ExpectedConditions
+                    .visibilityOfElementLocated(MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"Swipe horizontal\")")));
+
+
+            // Get Mobile window Size
+            Dimension windowSize = appiumDriver.manage().window().getSize();
+            int screenHeight = windowSize.getHeight();
+            int screenWidth = windowSize.getWidth();
+
+            // Calculate touch points
+            int xStartPoint = 50 * screenWidth / 100;
+            int xEndPoint = 10 * screenWidth / 100;
+
+            int yStartPoint = 70 * screenHeight / 100;
+            int yEndPoint = 70 * screenHeight / 100;
+
+            // Convert coordinates -> PointOption
+            PointOption startPoint = new PointOption<>().withCoordinates(xStartPoint, yStartPoint);
+            PointOption endPoint = new PointOption<>().withCoordinates(xEndPoint, yEndPoint);
+
+            // Using TouchAction to swipe
+            TouchAction touchAction = new TouchAction(appiumDriver);
+
+            // Swipe from right to left
+            for (int time = 0; time < 5; time++) {
+                touchAction
+                        .press(startPoint)
+                        .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                        .moveTo(endPoint)
+                        .release()
+                        .perform();
+            }
+
+            // Swipe from left to right
+            for (int time = 0; time < 5; time++) {
+                touchAction
+                        .press(endPoint)
+                        .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                        .moveTo(startPoint)
+                        .release()
+                        .perform();
+            }
+
+            // DEBUG PURPOSE ONLY
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        appiumDriver.quit();
+    }
+}
--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -41,8 +41,8 @@ public class DriverFactory implements MobileCapabilityTypeEx {
         }
 
         // Implicit wait | Interval time 500ms
-        appiumDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-
+//        appiumDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        appiumDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         return appiumDriver;
     }
 }
